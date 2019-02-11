3a6bdc1a72931dd8db8d1ff8bb6f00b52841521a
ddot727 <ddot727@gmail.com>
2019-02-11 18:10:18 -0500

    finished card domponents, moving on to styling

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import CardContainer from "./components/CardComponents/CardContainer";
+import CardContainer from './components/CardComponents/CardContainer';
 
 const App = () => {
   return (
-    <div>
+    <div className = 'App'>
       <HeaderContainer />
       <CardContainer />
     </div>
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () =>{
+    return (
+        <div className = 'cardContent'>
+            <h2>Get Started With React</h2>
+            <p>React makes it painless to creat interactive UIs. Design simple views for each state in you application.</p>
+        </div>
+    )
+}
+
+export default CardContent;
