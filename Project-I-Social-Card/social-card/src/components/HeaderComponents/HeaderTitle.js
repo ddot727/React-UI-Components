70ccff77b31eaab3b1d6cdcde6588012881b4af0
ddot727 <ddot727@gmail.com>
2019-02-11 17:48:37 -0500

    finished header components and set App.js up

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div className = "headerTitle">
+            <p>Lambda School <span> @Lambda School - 26 jan </span></p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
