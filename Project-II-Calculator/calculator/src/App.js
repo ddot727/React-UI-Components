b826ac00364569095679859c238fa28ad4df3dbb
ddot727 <ddot727@gmail.com>
2019-02-12 16:45:18 -0500

    finished styling buttons

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import CalculatorDisplay from './components/DisplayComponents';
 import NumberButton from './components/ButtonComponents/NumberButton'
-// import ActionButton from './components/ButtonComponents/ActionButton';
+import ActionButton from './components/ButtonComponents/ActionButton';
 
 import './App.css';
 
@@ -11,23 +11,24 @@ const App = () => {
       {/* <CalculatorDisplay /> */}
       <div className = 'buttons'>
         <div className = 'numbers'>
-       
-        <NumberButton buttonStyle={'number'} text={7} />
-        <NumberButton buttonStyle={'number'} text={8} />
-        <NumberButton buttonStyle={'number'} text={9} />
-        <NumberButton buttonStyle={'number'} text={4} />
-        <NumberButton buttonStyle={'number'} text={5} />
-        <NumberButton buttonStyle={'number'} text={6} />
-        <NumberButton buttonStyle={'number'} text={1} />
-        <NumberButton buttonStyle={'number'} text={2} />
-        <NumberButton buttonStyle={'number'} text={3} />
+          <ActionButton buttonStyle={'action'} text={'clear'}/>
+          <NumberButton buttonStyle={'number'} text={7} />
+          <NumberButton buttonStyle={'number'} text={8} />
+          <NumberButton buttonStyle={'number'} text={9} />
+          <NumberButton buttonStyle={'number'} text={4} />
+          <NumberButton buttonStyle={'number'} text={5} />
+          <NumberButton buttonStyle={'number'} text={6} />
+          <NumberButton buttonStyle={'number'} text={1} />
+          <NumberButton buttonStyle={'number'} text={2} />
+          <NumberButton buttonStyle={'number'} text={3} />
+          <ActionButton buttonStyle={'action'} text={0}/>
         </div>
         <div className='operator'>
-        <NumberButton buttonStyle={'operator'} text='/' />
-        <NumberButton buttonStyle={'operator'} text='*' />
-        <NumberButton buttonStyle={'operator'} text='-' />
-        <NumberButton buttonStyle={'operator'} text='+' />
-        <NumberButton buttonStyle={'operator'} text='=' />
+          <NumberButton buttonStyle={'operator'} text='/' />
+          <NumberButton buttonStyle={'operator'} text='*' />
+          <NumberButton buttonStyle={'operator'} text='-' />
+          <NumberButton buttonStyle={'operator'} text='+' />
+          <NumberButton buttonStyle={'operator'} text='=' />
         </div>
       </div>
     </div>
